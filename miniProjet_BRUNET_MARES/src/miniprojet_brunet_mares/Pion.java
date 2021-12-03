dcbdfbe63856beca6cb940654c9e345aac36d32b
egnma <egnma@192.168.115.171>
2021-12-03 11:08:27 +0100

    Classe jeton

--- miniProjet_BRUNET_MARES/src/miniprojet_brunet_mares/Pion.java
+++ miniProjet_BRUNET_MARES/src/miniprojet_brunet_mares/Pion.java
@@ -9,5 +9,20 @@ package miniprojet_brunet_mares;
  * @author egnma
  */
 public class Pion {
+    //Attributs
+    String COULEUR ; //Couleur du jeton (Rouge ou Bleu)
+    String TYPE ;
+    
+    //Méthodes :
+    public Pion(String couleur, String type) { //constructeur
+        COULEUR = couleur;
+        TYPE = type ;
+    }
+    public String lireType(){
+        return TYPE ;
+    }
+    public String lireCouleur(){ //Initialisation de la couleur du jeton aléatoirement
+        return COULEUR;
+    }
     
 }
