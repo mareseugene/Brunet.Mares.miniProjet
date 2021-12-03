33c8eda16813182fb00cefcafb83142d02d180ee
egnma <egnma@192.168.115.171>
2021-12-03 10:39:38 +0100

    CREATION PROJET

---
+++ miniProjet_BRUNET_MARES/src/miniprojet_brunet_mares/MiniProjet_BRUNET_MARES.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package miniprojet_brunet_mares;
+
+/**
+ *
+ * @author egnma
+ */
+public class MiniProjet_BRUNET_MARES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
