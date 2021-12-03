3a47a83cd1631210011a493e2f9b7a6339e70696
egnma <egnma@192.168.115.171>
2021-12-03 11:17:37 +0100

    Classe Joueur

--- miniProjet_BRUNET_MARES/src/miniprojet_brunet_mares/Joueur.java
+++ miniProjet_BRUNET_MARES/src/miniprojet_brunet_mares/Joueur.java
@@ -3,11 +3,29 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package miniprojet_brunet_mares;
-
+import java.util.ArrayList;
 /**
  *
  * @author egnma
  */
 public class Joueur {
+    
+    //Attributs
+    String Nom ;
+    String Couleur;
+    ArrayList<Piece> ListePieces = new ArrayList<Piece>() ;
+    
+    // Méthodes
+    public Joueur(String nom, String couleur){
+        Nom = nom;
+        Couleur = couleur;
+    }
+    
+    public void affecterCouleur(String Couleur){
+        this.Couleur = Couleur;
+        //On utilise this car le nom de la variable globale (Couleur)
+        // est le même que celui de la variable en argument de la méthode
+    }
+    
     
 }
