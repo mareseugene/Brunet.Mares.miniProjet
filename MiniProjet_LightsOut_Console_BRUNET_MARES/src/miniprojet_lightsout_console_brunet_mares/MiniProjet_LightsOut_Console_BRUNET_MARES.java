5d5a58d32fd6dbe17003d8cf305e317a2d8c2aae
Utilisateur <Utilisateur@DESKTOP-45UG25C>
2021-12-06 15:40:59 +0100

    LigthsOut Console

---
+++ MiniProjet_LightsOut_Console_BRUNET_MARES/src/miniprojet_lightsout_console_brunet_mares/MiniProjet_LightsOut_Console_BRUNET_MARES.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package miniprojet_lightsout_console_brunet_mares;
+
+/**
+ *
+ * @author Utilisateur
+ */
+public class MiniProjet_LightsOut_Console_BRUNET_MARES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
