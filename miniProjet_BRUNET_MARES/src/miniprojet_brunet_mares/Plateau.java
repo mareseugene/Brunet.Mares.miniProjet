9588b1f6be498a132c246208507f36e86019a1bc
egnma <egnma@192.168.115.171>
2021-12-06 14:25:33 +0100

    Classe carte

--- miniProjet_BRUNET_MARES/src/miniprojet_brunet_mares/Plateau.java
+++ miniProjet_BRUNET_MARES/src/miniprojet_brunet_mares/Plateau.java
@@ -10,9 +10,37 @@ package miniprojet_brunet_mares;
  */
 
 public class Plateau {
+    //Attributs : 
     
+    Plateau [][] PlateauJeu = new Plateau[5][5];
     
-     
+    public Plateau(){
+        
+    }
+    
+    public void initialiserPiece(){
+        // Creation de tous les pions
+        Piece Pion1J1 = new Piece("Rouge", "Pion") ;
+        Piece Pion2J1 = new Piece("Rouge", "Pion") ;
+        Piece Pion3J1 = new Piece("Rouge", "Pion") ;
+        Piece Pion4J1 = new Piece("Rouge", "Pion") ;
+        Piece Pion1J2 = new Piece("Bleu", "Pion") ;
+        Piece Pion2J2 = new Piece("Bleu", "Pion") ;
+        Piece Pion3J2 = new Piece("Bleu", "Pion") ;
+        Piece Pion4J2 = new Piece("Bleu", "Pion") ;
+        
+       // Creation des Rois :
+       Piece RoiJ1 = new Piece ("Rouge","Roi");
+       Piece RoiJ2 = new Piece ("Bleu","Roi");
+    }
+    
+        
+        
+    
+    public void piocherCarte(){ // Prendre la carte du deck pour la mettre devant le joueur
+        
+    }
+    
     
     
 }
