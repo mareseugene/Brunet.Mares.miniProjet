13a4c909997526e1542448ce3c1d11abeb785cab
egnma <egnma@LAPTOP-E8KNTUKE>
2021-12-10 09:30:08 +0100

    Class Case

---
+++ MiniProjet_LightsOut_Graphique_BRUNET_MARES/src/miniprojet_lightsout_graphique_brunet_mares/Case.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package miniprojet_lightsout_graphique_brunet_mares;
+
+/**
+ *
+ * @author egnma
+ */
+public class Case {
+    String Couleur ;
+    
+    public Case(String couleur){
+    Couleur = couleur ;
+}
+    
+    public String lireCouleur (){
+        return Couleur ;
+    }
+}
