898744d7548329eada7867477db3a847122967b5
egnma <egnma@LAPTOP-E8KNTUKE>
2022-01-04 14:54:15 +0100

    Récupération des classes, par mail car problème 	avec Github pour Christophe

--- MiniProjet_LightsOut_Console_BRUNET_MARES/src/miniprojet_lightsout_console_brunet_mares/MiniProjet_LightsOut_Console_BRUNET_MARES.java
+++ MiniProjet_LightsOut_Console_BRUNET_MARES/src/miniprojet_lightsout_console_brunet_mares/MiniProjet_LightsOut_Console_BRUNET_MARES.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package miniprojet_lightsout_console_brunet_mares;
-
+import java.util.Scanner;
 /**
  *
  * @author Utilisateur
@@ -14,7 +14,19 @@ public class MiniProjet_LightsOut_Console_BRUNET_MARES {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         
+        System.out.println("Rentrez votre pseudo : ");
+        Joueur joueur = new Joueur(sc.nextLine());
+        
+        
+        Partie partie1 = new Partie(joueur);
+        
+        // On lance toute la partie
+        
+        partie1.initialiserPartie();
+        
+        partie1.debuterPartie();
     }
     
 }
